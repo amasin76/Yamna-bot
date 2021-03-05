ee60b631be7192fdbaa9f1c0ce251e568299a032
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-03-05 23:28:40 +0100

    wlc log

--- handlers/welcome.js
+++ handlers/welcome.js
@@ -34,7 +34,6 @@ module.exports = client => {
     const guildInvites = new Map();
     client.on("inviteCreate", async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchInvites()));
     client.on("ready", () => {
-        console.log(`${client.user.tag} is Online`)
         client.guilds.cache.forEach(guild => {
             guild.fetchInvites()
                 .then(invites => guildInvites.set(guild.id, invites))
