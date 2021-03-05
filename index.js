7b213e7fb7995c8e01f45449dddc02ca9bbe9073
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-03-05 23:10:22 +0100

    logger

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let prefix = "~";
 /*require("./handler/module.js")(client);
 require("./handler/Event.js")(client);*/
 //Handler
-["module", "Event", "welcome", "distube", "chat"].forEach(handler => {
+["module", "Event", "welcome", "distube", "chat", "logger"].forEach(handler => {
     require(`./handlers/${handler}`)(client);
 });
 //client=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -32,7 +32,5 @@ client.package = require("./package.json");
 client.on("warn", console.warn); // This will warn you via logs if there was something wrong with your bot.
 client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
 client.login(process.env.SECRET).catch(console.error); // This token will leads to the .env file. It's safe in there.
-//Logger=-=-=-=-=-=-=-=-=-=-=-=-=
-require("./logger")(client);
 //client.login(process.env.SECRET)
 client.login(process.env.SECRET).catch(console.error);
