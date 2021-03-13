c927ed0299324f951366857b4a3727888fd75558
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-03-13 05:57:03 +0100

    pubg stats

--- commands/games/pubgRank.js
+++ commands/games/pubgRank.js
@@ -6,9 +6,9 @@ const { access_api } = process.env;
 exports.run = async (client, message, args) => {
     try {
         let name = args[0];
-        let mode = args[1];
-        console.log(name, mode)
-        const getId = (name, mode) => {
+        let season = parseInt(args[1]);
+        console.log(name, season)
+        const getId = (name, season) => {
             axios.get(`https://api.pubg.com/shards/steam/players?filter[playerNames]=${name}`, {
                 headers: {
                     'Authorization': `Bearer ${access_api}`,
@@ -16,12 +16,12 @@ exports.run = async (client, message, args) => {
                 }
             })
                 .then((res) => {
-                    //console.log(res.data.data[0].id)
                     const data = res.data.data[0].id
+                    //console.log(data)
                     return data;
                 })
                 .then((data) => {
-                    let resp = axios.get(`https://api.pubg.com/shards/steam/seasons/lifetime/gameMode/${mode}/players?filter[playerIds]=${data}&filter[gamepad]=false`, {
+                    let resp = axios.get(`https://api.pubg.com/shards/steam/players/${data}/seasons/division.bro.official.pc-2018-${season}/ranked`, {
                         headers: {
                             'Authorization': `Bearer ${access_api}`,
                             'Accept': 'application/vnd.api+json'
@@ -32,70 +32,55 @@ exports.run = async (client, message, args) => {
                     return resp;
                 })
                 .then((resp) => {
-                    let status = resp.status
-                    //console.log(status)
-                    let lifeStats = resp.data.data[0].attributes.gameModeStats[`${mode}`]
+                    //let status = resp.status
+                    let lifeStats = resp.data.data.attributes.rankedGameModeStats['squad-fpp']//.data[0].attributes.gameModeStats[`${mode}`]
+                    //console.log(lifeStats)
                     return message.channel.send(new MessageEmbed()
                         .setColor('#fcbe03')
                         .setFooter(config.footertext, config.footericon)
                         .setThumbnail('https://pngimg.com/uploads/pubg/pubg_PNG8.png')
-                        .setAuthor('PUBG STATS / LIFE-TIME', 'https://www.logolynx.com/images/logolynx/c3/c3ffc8726b01df955af0b9dadb1f7f13.png', 'https://discord.gg/CyH8avz')
+                        .setAuthor('PUBG STATS / Season', 'https://www.logolynx.com/images/logolynx/c3/c3ffc8726b01df955af0b9dadb1f7f13.png', 'https://discord.gg/CyH8avz')
                         //.setTitle(`LIFE-TIME STATS IN GAME`)
-                        .setDescription(`\`\`\` Player Name : ${name} || Mode : ${mode} \`\`\``)
+                        .setDescription(`\`\`\` Player Name : ${name} || Mode : ${season} \`\`\``)
                         .addFields(
                             {
-                                name: "🏁Rounds Played: ",
-                                value: ` ${Math.round(lifeStats.roundsPlayed)}`,
+                                name: "🔺Rounds Played ",
+                                value: ` ${lifeStats.roundsPlayed}`,
                                 inline: true
                             },
                             {
-                                name: "📆Days Played: ",
-                                value: ` ${Math.round(lifeStats.days)}`,
+                                name: "🏁Current Tier: ",
+                                value: ` __${lifeStats.currentTier.tier} ${Math.round(lifeStats.currentTier.subTier)}__`,
                                 inline: true
                             },
                             {
-                                name: "💀Kills: ",
-                                value: ` ${Math.round(lifeStats.kills)}`,
+                                name: "📆Current Rank Point: ",
+                                value: ` ${lifeStats.currentRankPoint}`,
                                 inline: true
                             },
                             {
-                                name: "🔺Most Kills: ",
-                                value: ` ${Math.round(lifeStats.roundMostKills)}`,
+                                name: "💀Best Tier: ",
+                                value: ` ${lifeStats.bestTier.tier} ${lifeStats.bestTier.subTier}`,
                                 inline: true
                             },
                             {
-                                name: "🎯Longest Kill: ",
-                                value: ` ${Math.round(lifeStats.longestKill)}`,
+                                name: "💀Best Rank Point: ",
+                                value: ` ${lifeStats.bestRankPoint} ${lifeStats.bestRankPoint}`,
                                 inline: true
                             },
                             {
-                                name: "🚦Road Kills: ",
-                                value: ` ${Math.round(lifeStats.roadKills)}`,
+                                name: "🎯Wins: ",
+                                value: ` ${lifeStats.wins}`,
                                 inline: true
                             },
                             {
-                                name: "💔Team Kill: ",
-                                value: ` ${Math.round(lifeStats.teamKills)}`,
+                                name: "🚨winRatio: ",
+                                value: ` ${lifeStats.winRatio.toFixed(3)}`,
                                 inline: true
                             },
                             {
-                                name: "💣Suicides: ",
-                                value: ` ${Math.round(lifeStats.suicides)}`,
-                                inline: true
-                            },
-                            {
-                                name: "💥Vehicle Destroys: ",
-                                value: ` ${Math.round(lifeStats.vehicleDestroys)}`,
-                                inline: true
-                            },
-                            {
-                                name: "👣Walk Distance: ",
-                                value: ` ${Math.round((lifeStats.walkDistance) / 1000)} KM`,
-                                inline: true
-                            },
-                            {
-                                name: "🚗Ride Distance: ",
-                                value: ` ${Math.round((lifeStats.rideDistance) / 1000)} KM`,
+                                name: "💔K/D: ",
+                                value: ` ${lifeStats.kda.toFixed(3)}`,
                                 inline: true
                             }
                         )
@@ -110,31 +95,31 @@ exports.run = async (client, message, args) => {
                             .setColor(config.wrongcolor)
                             .setFooter(config.footertext, config.footericon)
                             .setDescription(`❌ **ERROR ${statusErr}** : 	
-                            API key invalid or missing`))
+                        API key invalid or missing`))
                     } else if (statusErr == 404) {
                         return message.channel.send(new MessageEmbed()
                             .setColor(config.wrongcolor)
                             .setFooter(config.footertext, config.footericon)
                             .setDescription(`❌ **ERROR ${statusErr}** : 	
-                            The specified resource was not found, The inputs not valid (username,mode..)`))
+                        The specified resource was not found, The inputs not valid (username,mode..)`))
                     } else if (statusErr == 415) {
                         return message.channel.send(new MessageEmbed()
                             .setColor(config.wrongcolor)
                             .setFooter(config.footertext, config.footericon)
                             .setDescription(`❌ **ERROR ${statusErr}** : 	
-                            Content type incorrect or not specified`))
+                        Content type incorrect or not specified`))
                     } else if (statusErr == 415) {
                         return message.channel.send(new MessageEmbed()
                             .setColor(config.wrongcolor)
                             .setFooter(config.footertext, config.footericon)
                             .setDescription(`❌ **ERROR ${statusErr}** : 	
-                            Content type incorrect or not specified`))
+                        Content type incorrect or not specified`))
                     } else if (statusErr == 429) {
                         return message.channel.send(new MessageEmbed()
                             .setColor(config.wrongcolor)
                             .setFooter(config.footertext, config.footericon)
                             .setDescription(`❌ **ERROR ${statusErr}** : 	
-                            Too many requests`))
+                        Too many requests`))
                     } else {
                         return message.channel.send(new MessageEmbed()
                             .setColor(config.wrongcolor)
@@ -146,14 +131,14 @@ exports.run = async (client, message, args) => {
 
             //return lifeStats = resp.data.data[0].attributes.gameModeStats.mode
         }
-        getId(name, mode)
+        getId(name, season)
     } catch (err) {
         console.log(err)
     }
 };
 exports.help = {
-    name: "pubg",
-    description: "Get pubg life-time stats",
+    name: "pubg-rank",
+    description: "Get pubg Rank stats",
     usage: "<prefix>pubg <player name><mode>",
     example: "=pubg cubevilly squad-fpp"
 };
