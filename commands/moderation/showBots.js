5ff548082fdf6ccbf06d5564fbd0ac2f993ae198
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-03-31 16:09:55 +0100

    server bots

---
+++ commands/moderation/showBots.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require('discord.js')
+
+exports.run = async (client, message, args) => {
+    const botsSize = message.guild.members.cache.filter(m => m.user.bot).map(m => `<@${m.id}> [ ${m.user.username} ]`);
+    const x = new MessageEmbed()
+        .setColor('GREEN')
+        .setDescription(`${botsSize.join('\n \━\━\━\━\━\━\━\━\━\━\━\━\━\━\━\━\━\━\ \n')}`)
+        .setFooter(`Total Bots : ${message.guild.members.cache.filter(member => member.user.bot).size}`)
+    message.channel.send(x)
+}
+exports.help = {
+    name: "showBots",
+    description: "See the total number of bots in a server",
+    usage: "<prefix>ls",
+    example: "=ls"
+}
+exports.conf = {
+    aliases: ["showbots", "ls"],
+    cooldown: 5
+}
