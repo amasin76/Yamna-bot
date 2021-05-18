44aa04c2e364ab7fa732333fecfc0711a7a803e1
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-05-18 09:25:49 +0100

    bot cmd

--- events/message.js
+++ events/message.js
@@ -30,7 +30,7 @@ module.exports = async (client, message) => {
     try {
         if (message.author.bot) return;
         if (message.channel.id == '749584245457944577' && message.content.toLowerCase().includes('clips.twitch')) return;
-        if ((message.channel.id !== '746777186714779770' || message.channel.id !== '717058723692019772') && shareLinks.some(word => message.content.toLowerCase().includes(word))) {
+        if ((message.channel.id !== '746777186714779770' || '717058723692019772') && shareLinks.some(word => message.content.toLowerCase().includes(word))) {
             await message.delete();
             return await message.channel.send(new MessageEmbed()
                 .setColor(config.wrongcolor)
